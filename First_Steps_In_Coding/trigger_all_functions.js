daff4d11e66bb195b605cd15beac432a05c3179d
achi200 <80904433+achi200@users.noreply.github.com>
2024-06-08 12:27:36 +0300

    Create trigger_all_functions.js to call all other functions

---
+++ First_Steps_In_Coding/trigger_all_functions.js
@@ -0,0 +1,14 @@
+function triggerAllFunctions() {
+    hello();
+    numbersFromOneToTen();
+    findSquareArea(["5"]);
+    inchesToCentimeters(['5']);
+    greetings(["dido"]);
+    concatenateData(["Dido", "Kalaydzhiev", 20, "Blagoevgrad"]);
+    projectCreation(["Dido", "4"]);
+    zooShop(["5", "4"]);
+    yardGardening(["550"]);
+    helloWorld();
+}
+
+triggerAllFunctions();
